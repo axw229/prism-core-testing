2382fe4113d016a7ab9e3045ffe182f080926c4d
Sam Thompson <sam.thompson@sherwin.com>
2022-10-06 14:29:00 -0500

    chore: DCT-795: upgrading storybook
    - removing unused dependencies and scripts
    - pinning package versions
    - updating webpack version
    - adding storybook a11y addon

--- packages/toolkit/.storybook/main.js
+++ packages/toolkit/.storybook/main.js
@@ -1,9 +1,16 @@
 const path = require('path')
 
 module.exports = {
-  stories: ['../src/components/**/*.stories.@(tsx|mdx)', '../src/stories/**/*.stories.@(tsx|mdx)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: ['@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    './addons/testTab/register',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-a11y'
+  ],
+  framework: '@storybook/react',
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\,css&/,
